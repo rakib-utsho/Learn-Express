49221fa5de55a9c61f62cfa48f90f2913e981a11
rakib-utsho <rakibutsho1920@gmail.com>
2024-12-08 11:31:42 +0600

    Explore Express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// console.dir(app);
+
+let port = 3000; //Ports are the logical endpoints of a network connection that is used to exchange information between a web server and web client.
+
+app.listen(port, () =>{
+    console.log(`app is listening on port ${port}`);
+})
